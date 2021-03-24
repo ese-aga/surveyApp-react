3980ebcbb6c98e80a07360d8dbdef73a9e470d57
Kelvin Yoreme <kelvinyoreme@Kelvins-MacBook-Pro.local>
2021-03-24 04:28:12 +0100

    Added Billing and client side app

---
+++ client/src/components/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>
+        FeedBack App
+      </h1>
+      Collect feedback from users
+    </div>
+  );
+};
+
+export default Landing
